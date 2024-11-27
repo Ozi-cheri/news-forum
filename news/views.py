ed1ecb59ec105aae2f5559c9cff265a73718ad12
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-11-27 18:56:04 +0000

    Update news app views to render templates

--- news/views.py
+++ news/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('<h1>Welcome to the News Forum!</h1>')
-
+def news_list(request):
+    return render(request, 'news/news_list.html')
