60658d06c24b9d635f9eccf79241a3d30e55abbf
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-11-27 19:04:16 +0000

    Add url mapping for news_list view in news app

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'news'
+
 urlpatterns = [
-    path('', views.home, name='home'),  
+    path('', views.news_list, name='news_list'),  
 ]
