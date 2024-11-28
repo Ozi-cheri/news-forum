1e87d0e098c4cca50fc94d35bbc678e37fd36faa
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-11-28 15:05:13 +0000

    Update views.py in news app

--- news/views.py
+++ news/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def news_list(request):
+    return render(request, 'news/news_list.html')
