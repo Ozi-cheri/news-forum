f517ebe873b8a0ec1e9f2add84250c217cb2dc91
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-11-30 16:01:37 +0000

    Add news to admin.py

--- news/admin.py
+++ news/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Post
+from .models import News
 
-admin.site.register(Post)
+admin.site.register(News)
