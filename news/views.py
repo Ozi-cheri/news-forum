00563e1a09c6bc036630a4d93edb55efdd3c1956
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-03 20:14:55 +0000

    Update views.py, news_list.html and create base.html file

--- news/views.py
+++ news/views.py
@@ -6,8 +6,12 @@ from .models import Post
 def news_list(request):
 
     news_items = Post.objects.all().order_by('-created_at')
-    paginator = Paginator(news_items, 10)
+    paginator = Paginator(news_items, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'news/news_list.html',{'news_items': news_items})
+    return render(request, 'news/news_list.html', {
+        'news_items': page_obj.object_list,
+        'has_next_page': page_obj.has_next(),
+        'page_obj': page_obj,
+    })
