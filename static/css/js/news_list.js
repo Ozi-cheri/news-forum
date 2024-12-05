b13b13fc8607b7f2e038cc2e5aeb79193441eb8a
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-05 16:28:34 +0000

    Modify news_list.js file

--- static/css/js/news_list.js
+++ static/css/js/news_list.js
@@ -1,26 +1,35 @@
-const loadMoreBtn = document.getElementById('load-more-btn');
-let currentPage = 1;
+document.addEventListener('DOMContentLoaded', () => {
+    const loadMoreBtn = document.getElementById('load-more-btn');
+    let currentPage = 1;
 
-if (loadMoreBtn) {
-    loadMoreBtn.addEventListener('click', () => {
-        currentPage += 1;
-        fetch(`?page=${currentPage}`)
-            .then(response => response.text())
-            .then(data => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(data, 'text/html');
-                const newItems = doc.querySelectorAll('#news-list li');
-                const newsList = document.getElementById('news-list');
-
-                newItems.forEach(item => {
-                    newsList.appendChild(item);
-                });
-
-                const newLoadMoreBtn = doc.querySelector('#load-more-btn');
-                if (!newLoadMoreBtn) {
-                    loadMoreBtn.style.display = 'none';
-                }
+    if (loadMoreBtn) {
+        loadMoreBtn.addEventListener('click', () => {
+            currentPage += 1;
+            fetch(`?page=${currentPage}`, {
+                method: 'GET',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                },
             })
-            .catch(error => console.error('Error fetching more news:', error));
-    });
-}
+                .then((response) => response.text())
+                .then((data) => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(data, 'text/html');
+                    const newItems = doc.querySelectorAll('#news-list li');
+                    const newsList = document.getElementById('news-list');
+
+                    newItems.forEach((item) => {
+                        newsList.appendChild(item);
+                    });
+
+                    const newLoadMoreBtn = doc.querySelector('#load-more-btn');
+                    if (!newLoadMoreBtn) {
+                        loadMoreBtn.style.display = 'none';
+                    }
+                })
+                .catch((error) =>
+                    console.error('Error fetching more news:', error)
+                );
+        });
+    }
+});
