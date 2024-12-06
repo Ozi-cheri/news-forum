7b618ab051a0d7d6c2fed3dd01a5422fac37137a
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-12-06 16:51:12 +0000

    Modify views.py in news app

--- news/views.py
+++ news/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
-from .models import Post 
+
 # Create your views here.     
  
-def news_list(request):
-    """
-    View for displaying a paginated list of news items.
-    """
-    news_items = Post.objects.all().order_by('-created_at')  
-    paginator = Paginator(news_items, 5)  
-    page_number = request.GET.get('page')  
-    page_obj = paginator.get_page(page_number)  
-
+def home(request):
     
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest': 
-        return render(request, 'news/news_items.html', {
-            'news_items': page_obj.object_list,
-        })
-
-    
-    return render(request, 'news/news_list.html', {
-        'news_items': page_obj.object_list,
-        'has_next_page': page_obj.has_next(),  
-    })
+    return render(request, 'news/home.html')  
+        
+ 
